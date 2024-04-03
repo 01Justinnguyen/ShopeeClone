b5a716acf7d7515cb3647ce5848437d13c0c54b6
Liam <phuc01112002@gmail.com>
2024-04-03 10:19:28 +0700

    define profile API

---
+++ src/api/user.api.ts
@@ -0,0 +1,28 @@
+import { User } from '@/types/user.types'
+import { SuccessResponseApi } from '@/types/utils.types'
+import http from '@/utils/http'
+
+interface BodyUpdate extends Omit<User, '_id' | 'roles' | 'createAt' | 'updatedAt' | 'email'> {
+  password?: string
+  newPassword?: string
+}
+
+const UserApi = {
+  getProfile() {
+    return http.get<SuccessResponseApi<User>>('/me')
+  },
+
+  updateProfile(body: BodyUpdate) {
+    return http.put<SuccessResponseApi<User>>('/user', body)
+  },
+
+  uploadAvatar(body: FormData) {
+    return http.post<SuccessResponseApi<string>>('/user/upload-avatar', body, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+  }
+}
+
+export default UserApi
